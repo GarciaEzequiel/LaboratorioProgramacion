4c31ebb294523aa407c2fd5c67fdebc95762ef1b
GarciaEzequiel <EZEGARCIAIES@ies21.onmicrosoft.com>
2025-04-21 22:08:46 -0300

    "Actualización del mensaje de bienvenida"

--- LaboratorioProgramacion/Program.cs
+++ LaboratorioProgramacion/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("¡Lindo viernes para estar con Visual Studio 2025!");
+//Console.WriteLine("¡Lindo viernes para estar con Visual Studio 2025!");
+Console.WriteLine("¡Primer cambio en el código, subiéndo a GitHub!");
 
